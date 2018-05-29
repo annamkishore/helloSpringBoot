4087bbded38458c154bec8b0a66b73b7e23a7dd7
Kishore Annam <annamkishore@gmail.com>
2018-05-29 22:23:06 +0530

    Employee DTO

---
+++ src/main/java/kish/dto/Employee.java
@@ -0,0 +1,51 @@
+package kish.dto;
+
+import java.util.Date;
+
+public class Employee {
+    String employeeId;
+    String employeeName;
+    String email;
+    String phone;
+    Date joiningDate;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getJoiningDate() {
+        return joiningDate;
+    }
+
+    public void setJoiningDate(Date joiningDate) {
+        this.joiningDate = joiningDate;
+    }
+}

f917a8f8ee48377e1cac6ab57d734b4cd05a52b3
Kishore Annam <annamkishore@gmail.com>
2018-05-30 15:26:47 +0530

    get, post, put

--- src/main/java/kish/controllers/HelloController.java
+++ src/main/java/kish/controllers/HelloController.java
@@ -1,9 +1,7 @@
 package kish.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import kish.dto.EmployeeDto;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -11,11 +9,21 @@ public class HelloController {
 
     @PostMapping("/hello")
     public String home() {
-        return "Hello World 2";
+        return "Hello World";
     }
 
     @GetMapping("/wish")
     public String service1() {
         return "Good Morning";
+    }
+
+    @PutMapping("/employeeAdd")
+    public Boolean addEmployee(@RequestBody EmployeeDto employeeDto) {
+        return true;
+    }
+
+    @GetMapping("/employeeGet/{employeeId}")
+    public EmployeeDto getEmployee(String employeeId) {
+        return null;
     }
 }
